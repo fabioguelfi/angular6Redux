2f95cb331fa445ff7d55b2c4da368ceaa5e6b8cd
fabioguelfi <fabioguelfiunix@gmail.com>
2018-06-04 08:45:41 -0300

    define initial state

---
+++ src/app/store/app.state.ts
@@ -0,0 +1,23 @@
+import { User } from '../models/user';
+import * as auth from './reducers/auth.reducers';
+
+export interface State {
+    // is a user authenticated?
+    isAuthenticated: boolean;
+    // if authenticated, there should be a user object
+    user: User | null;
+    // error message
+    errorMessage: string | null;
+    // load
+    // load: boolean;
+}
+
+export const initialState = {
+    isAuthenticated: false,
+    user: null,
+    errorMessage: null,
+}
+
+export interface AppState {
+    authState: auth.State;
+}
